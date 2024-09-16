3ed748e649a9799d8d260cbd563cf44a0a7da60f
venkata-naveen-varma <venkata2naveen@gmail.com>
2024-09-16 17:59:21 -0400

    multer profile picture

--- backend/index.js
+++ backend/index.js
@@ -30,10 +30,10 @@ app.use("/api/v1/user", userRoute);
 // app.use("/api/v1/post", postRoute);
 // app.use("/api/v1/message", messageRoute);
 
-// app.use(express.static(path.join(__dirname, "/frontend/dist")));
-// app.get("*", (req,res)=>{
-//     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-// })
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get("*", (req,res)=>{
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+})
 
 app.listen(PORT, () => {
     connectDB();
